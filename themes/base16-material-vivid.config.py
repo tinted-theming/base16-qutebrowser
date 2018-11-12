02581aa78e4c753f043fca626c2f813ff236ae7d
theova <theova@users.noreply.github.com>
2018-11-12 13:40:11 +0100

    Update base16-material-vivid

--- themes/base16-material-vivid.config.py
+++ themes/base16-material-vivid.config.py
@@ -2,22 +2,22 @@
 # Base16 qutebrowser template by theova
 # Material Vivid scheme by joshyrobot
 
-base00 = "#263238"
-base01 = "#37474F"
-base02 = "#455A64"
-base03 = "#546E7A"
-base04 = "#90A4AE"
-base05 = "#B0BEC5"
-base06 = "#CFD8DC"
-base07 = "#ECEFF1"
-base08 = "#FF1744"
-base09 = "#FF9100"
-base0A = "#FFC400"
-base0B = "#00E676"
-base0C = "#00B0FF"
-base0D = "#2979FF"
-base0E = "#651FFF"
-base0F = "#8D6E63"
+base00 = "#202124"
+base01 = "#27292c"
+base02 = "#323639"
+base03 = "#44464d"
+base04 = "#676c71"
+base05 = "#80868b"
+base06 = "#9e9e9e"
+base07 = "#ffffff"
+base08 = "#f44336"
+base09 = "#ff9800"
+base0A = "#ffeb3b"
+base0B = "#00e676"
+base0C = "#00bcd4"
+base0D = "#2196f3"
+base0E = "#673ab7"
+base0F = "#8d6e63"
 
 # set qutebrowser colors
 
